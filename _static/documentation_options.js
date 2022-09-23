21bde21c564f2a69b63745061c7c373378909ae0
benmaddison <benm@workonline.africa>
2022-09-23 18:20:09 +0000

    Deploying to gh-pages from @ job/draft-rpki-doa@5b9fe059e1ed5d3b78b080b3b300627c7a9d977b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.3.dev46',
+    VERSION: '0.0.3.dev45',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
